9e2bceedf65181dab85507f94adf777de148a1d5
Niccolò <granieriniccolo@gmail.com>
2018-09-23 11:15:43 +0100

    Update to V2
    Temp update

--- YT-MosherV2.py
+++ YT-MosherV2.py
@@ -12,10 +12,14 @@ from moviepy.editor import *
 
 word_array = ['ASMR', 'pink noise', 'vine complation', 'slow tv']
 
+clipArr = []
+
 for x in range(0, len(word_array)):
     txt_clip = TextClip(word_array[x], fontsize=100, color='white').set_pos("bottom")
     # finishedVideo = VideoFileClip('Output'+str(iterations-1)+'.mp4')
-    finishedVideo = VideoFileClip("/Users/Nico/Desktop/YTGlitch/Sqr.mp4")
+    finishedVideo = VideoFileClip("/Users/Nico/YT-Mosher/First Output.mp4")
     videolength = finishedVideo.duration
     subbedVideo = CompositeVideoClip([finishedVideo, txt_clip])
-    subbedVideo.subclip(x, x+1).write_videofile('Subbed' + str(x) + '.mp4')
+    clipArr.append(subbedVideo.subclip(x, x+1))
+final_clipz = concatenate_videoclips(clipArr)
+final_clipz.write_videofile('FinalSubbed.mp4')
